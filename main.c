524e47db92b91b904e2eac1ae8cb0f24847bf822
Kashishvishnoi99 <kashishvishnoi821@gmail.com>
2025-10-23 01:17:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
-#include<stdio.h>
-#include"arraylib.h"
+#include <stdio.h>
+#include "arraylib.h.h"
 
 int main(void){
 	int a[]={3,1,4,5};
 	int n=4;
 	DisplayArray(a,n);
 	FindMaxIndex(a,n);
-	FindMaxIndex(a,n);
+	FindMixIndex(a,n);
+	FindAverage(a,n);
 	ReverseArray(a,n);
 	DisplayArray(a,n);
 	SortArray(a,n);
 	DisplayArray(a,n);
 	LinearSearch(a,n,3);
-	LnearSearch(a,n,10);
+	LinearSearch(a,n,10);
 	
 	return 0;
 }
+
