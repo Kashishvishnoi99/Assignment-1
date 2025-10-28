8758714d481f1d9bcf46c700af60c99a5bfb468c
Kashishvishnoi99 <kashishvishnoi821@gmail.com>
2025-10-28 22:12:42 +0530

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -1,8 +1,9 @@
 #ifndef MYLIB_H
 #define MYLIB_H
+int reverseDigits(int n);
 int isArmstrong(int num);
 int isAdams(int num);
-int isPrimePalindrome(int num);
-int reverseDigits(int n);
 int isPrime(int num);
-#endif;
+int isPrimePalindrome(int num);
+#endif
+
